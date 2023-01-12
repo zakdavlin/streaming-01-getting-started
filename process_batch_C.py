691d6374696dbb1ed82de9da3c6452dffac9d9da
zakdavlin <105325923+zakdavlin@users.noreply.github.com>
2023-01-11 18:27:27 -0600

    Finished Program C to change K to F

--- process_batch_C.py
+++ process_batch_C.py
@@ -12,3 +12,20 @@ import csv
 input_file_name = "batchfile_2_kelvin.csv"
 output_file_name = "batchfile_3_farenheit.csv"
 
+input_file = open(input_file_name, "r")
+output_file = open(output_file_name, "w", newline='')
+
+reader = csv.reader(input_file, delimiter=",")
+writer = csv.writer(output_file, delimiter=",")
+
+header = next(reader)
+header_list = ["Year","Month","Day","Time","TempF"]
+writer.writerow(header_list)
+
+for row in reader:
+    Year, Month, Day, Time, TempK = row
+    TempF = round(((float(TempK))-(273.15))*(9/5) + 32,2)
+    writer.writerow([Year, Month, Day, Time, TempF])
+
+output_file.close()
+input_file.close()
